c8b23915669bb02260cea3f5f5b8f2cb08463a9f
noobandy <noobandy1364@gmail.com>
2015-11-26 16:52:07 +0530

    using translate directive where possible

--- public/index-app/javascripts/states.js
+++ public/index-app/javascripts/states.js
@@ -1,6 +1,11 @@
 "use strict";
 
-angular.module("indexApp").config(["$stateProvider", function($stateProvider) {
+angular.module("indexApp").config(["$stateProvider", "$urlRouterProvider", 
+	function($stateProvider, $urlRouterProvider) {
+	
+	// For any unmatched url, redirect to /
+  	$urlRouterProvider.otherwise("/");
+
 	$stateProvider.state({
 		name : "home",
 		url : "/",
